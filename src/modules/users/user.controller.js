0869a5b438ddc828d9e2f85a31eb49d06a1cf20d
RubenOAlvarado <rubenosmaralvarado@gmail.com>
2020-04-14 17:50:14 -0500

    cambios al signup: hash del password y validaciones

--- src/modules/users/user.controller.js
+++ src/modules/users/user.controller.js
@@ -5,6 +5,9 @@ export async function signUp(req, res){
         const user = await User.create(req.body);
         return res.status(201).json(user);
     } catch (error) {
+        console.log('====================================');
+        console.log(error);
+        console.log('====================================');
         return res.status(500).json(error);
     }
 }
--- src/modules/users/user.routes.js
+++ src/modules/users/user.routes.js
@@ -1,8 +1,12 @@
-import {Router} from 'express';
+import {
+    Router
+} from 'express';
+import validate from 'express-validation';
 import * as userController from './user.controller';
+import userValidation from './user.validations';
 
 const routes = new Router();
 
-routes.post('/signup', userController.signUp);
+routes.post('/signup', validate(userValidation.signup), userController.signUp);
 
 export default routes;
